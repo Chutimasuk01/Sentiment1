5caee061a93091562141941eb5cbd5ba8e45c2b2
Chutimasuk01 <chutima18092544@gmail.com>
2023-10-10 12:06:51 +0700

    commit

---
+++ Home.py
@@ -0,0 +1,21 @@
+import json
+import time
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
+st.subheader("Kairung Hengpraprohm")
+st.image('./pic/kairung.jpg')
+
+lot3="https://lottie.host/2e099151-4397-40ad-ae7b-249bbaf1e8df/gluEhZplO8.json"
+lottie3 = load_lottieurl(lot3)
+st_lottie(lottie3)
+st.balloons()
